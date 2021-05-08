70b9b5f81b4612022b5ebd90702f3aae11a7fa65
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-05-08 16:38:01 +0530

    feat(ui): add navbar

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Hentry`,
+    description: `Monitor hacks and ensure a free and fair playing field for all partitions.`,
+    author: `@yashkumarverma`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-postcss`,
-    
+
     {
       resolve: `gatsby-source-filesystem`,
       options: {
